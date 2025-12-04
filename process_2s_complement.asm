fec3bcbe00393b692f28676ca36a9e8da5f8fc62
Ansuperope <aspenfbn@yahoo.com>
2025-12-03 16:14:50 -0800

    setting up (no code yet)

---
+++ process_2s_complement.asm
@@ -0,0 +1,17 @@
+// ==============================================
+// OVERVIEW
+// 
+// ----------------------------------------------
+// DATA TABLE
+//  Inputs (Receieve):
+//      RAM[0]... RAM[14] - binary values from input
+//
+//  Variables:
+//      RAM[0], x1 holds x / lcv for first mult
+//      RAM[1], y1 holds y / lcv for second mult
+//      RAM[2], z1 holds x*x result
+// 
+//  Output (Send):
+//      RAM[5], z product x^y is stored in 
+// ==============================================
+(ac_process_2s_complement)
