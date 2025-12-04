2ee6a65a53d21a8b65996bb2e716d96f983532f5
Ansuperope <aspenfbn@yahoo.com>
2025-12-04 11:30:30 -0800

    get sign done
    basic if function, checks a value and preforms specific tasks if it passes. Will be using this as a templated function so I dont need to keep rewriting check functions
    
    will need to do:
    - put 2s complement in negative section
    
    - change next to jump to converting binary to decimal function

--- ac_process_get_sign.asm
+++ ac_process_get_sign.asm
@@ -1,17 +1,55 @@
 // ==============================================
 // ac_process_get_sign
+// flowchart: process_get_sign
 // ----------------------------------------------
 // OVERVIEW
-//  LOREM
+//  This function is a templated function. It will
+// be used in multiple locations. It looks at the
+// sign of our binary word and will preform
+// specific tasks based off of the sign value:
+//      0 = positive
+//      1 = negative
 // ----------------------------------------------
 // DATA TABLE
 //  Inputs:
-//      VAR - LOREM
+//      RAM[15]     - value checking, holds sign of binary word
 //
 //  Output:
-//      VAR - LOREM
+//      FUNC_POS    - where to jump if sign is positive (0)
+//      FUNC_NEG    - where to jump if sign is negative (1)
 //
-//  Temporary Variables:
-//      VAR - LOREM
+//  Variables:
+//      NEXT        - jumps to next part (continues program)
 // ==============================================
 (ac_process_get_sign)
+
+// ========== CHECK SIGN VALUE ==========
+@R15        // binary sign, value to check
+M=1         // R15 = 0 (neg) delete when done for testing only
+D=M
+
+@FUNC_POS   // where to jump to
+D;JEQ       // if R15 == 0 (positive) jump
+// ========== END CHECK SIGN VALUE ==========
+
+// ========== SIGN 1 / NEGATIVE ==========
+(FUNC_NEG)
+
+// ----- insert code here -----
+
+// ----- jump to next part -----
+@NEXT
+0;JMP
+// ========== END SIGN 1 / NEGATIVE ==========
+
+// ========== SIGN 0 / POS - JUMP TO ==========
+(FUNC_POS)
+// ----- insert code here -----
+// ========== END SIGN 0 / POS - JUMP TO ==========
+
+// ========== NEXT - REPLACE ==========
+(NEXT)
+
+// ----- replace when using -----
+@NEXT
+0;JMP
