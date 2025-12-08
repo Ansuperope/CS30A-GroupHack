1b924cf3ad8d866522408b8859355dad370afb93
Ansuperope <aspenfbn@yahoo.com>
2025-12-08 14:30:52 -0800

    cleaned files + merge 2s comp and add binary

--- process_2s_complement.asm
+++ process_2s_complement.asm
@@ -6,26 +6,6 @@
 //  This function will preform the 2s complement:
 //      1. it flips all the bits (done here)
 //      2. adds 1 (done in process_add_binary)
-// ----------------------------------------------
-// DATA TABLE
-//  Inputs (Receieve from input):
-//      R[0 - 14]   - binary values from input
-//                  0 is the first number
-//                  14 is the last number
-//
-//  Output (Send to next step):
-//      IR[0 - 14]  - opposite binary values from input
-//                  0 is the first number
-//                  14 is the last number
-//
-//  Variables:
-//      ac_index    - counter of how many times we
-//                  looped / index of array
-//
-//      ac_last_num - lenght of array / how many 
-//                  times to loop
-//
-//      NEXT        - next step of program, replace
 // ==============================================
 (ac_process_2s_complement)
 // ----- INITIALIZATIONS -----
@@ -50,7 +30,7 @@ D=M
 @ac_last_num
 D=M-D
 
-@NEXT
+@ac_process_add_binary
 D;JLE
 // ----- END CHECK IF ac_index < ac_last_num (15) ----- 
 
@@ -94,7 +74,106 @@ M=M+1
 0;JMP
 // ========== END FLIP BITS FOR LOOP ==========
 
-// ========== DONE - ac_process_2s_complement ==========
+
+// ==============================================
+// ac_process_add_binary
+// flowchart: process_add_binary
+// ----------------------------------------------
+// OVERVIEW
+//  This function is a part of the process to preform
+// 2s complement on a number. It adds 1 to a binary
+// number. After it adds it will check:
+//      1. if if the word overflows
+//      2. if there's any carries
+// ==============================================
+(ac_process_add_binary)
+
+// ========== CHECK FOR CARRIES ==========
+// ----- INITIALIZATIONS -----
+// ac_index = 0
+@ac_index
+M=0
+
+// ac_last_num = 15
+@15
+D=A
+@ac_last_num
+M=D
+// ----- END INITIALIZATIONS -----
+
+// ========== ADD 1 TO FIRST DIGIT ==========
+@IR0
+M=M+1
+
+// ===== CHECK IF CARRY =====
+(AC_CHECK_CARRY)
+
+// ----- CHECK IF ac_index < ac_last_num (15) -----
+@ac_index
+D=M
+
+@ac_last_num
+D=M-D
+
+// exit loop
+@NEXT
+D;JLE
+// ----- END CHECK IF ac_index < ac_last_num (15) ----- 
+
+// ----- CHECK IF IR[index] == 2 (CARRY) -----
+@ac_index
+D=M
+@IR0
+A=D+A       // IR[index]
+D=M         // D = IR[index]
+
+@1
+D=D-A
+@AC_ADD_BINARY_INC_INDEX
+D;JLE
+// ----- END CHECK IF IR[index] == 2 (CARRY) -----
+
+// ----- if IR[index] == 2 then IR[index] = 0 -----
+@ac_index
+D=M
+@IR0
+A=D+A       // IR[index]
+M=0
+
+// ===== IF index = 14 / ON LAST DIGIT / OVERFLOW =====
+@ac_index
+D=M
+
+@14
+D=D-A   // index - 14
+
+// index == 14
+@AC_ADD_BINARY_INC_INDEX
+D;JEQ
+// ===== END IF index = 14 / ON LAST DIGIT / OVERFLOW =====
+
+// ----- add 1 to next binary digit -----
+@ac_index
+D=M
+@1
+D=D+A
+@IR0
+A=D+A       // A = address of IR[index+1]
+M=M+1       // IR[index]++
+
+// ----- incrememnt ac_index -----
+(AC_ADD_BINARY_INC_INDEX)
+@ac_index
+M=M+1
+
+// ----- loop again -----
+@AC_CHECK_CARRY
+0;JMP
+// ===== END CHECK IF CARRY =====
+
+// ========== END CHECK FOR CARRIES ==========
+
+// ========== DONE - ac_process_add_binary ==========
 (NEXT)
 
 // ----- replace when using -----
